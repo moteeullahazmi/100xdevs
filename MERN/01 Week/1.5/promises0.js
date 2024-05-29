b35fa11bdee99310b231b6690b5737477b1593ef
Moteeullah <MY_NAME@example.com>
2024-05-29 18:26:33 +0530

    Week 1 complete

---
+++ MERN/01 Week/1.5/promises0.js
@@ -0,0 +1,3 @@
+let object = new Promise((reject,resolve)=>{
+
+})
---
+++ MERN/01 Week/1.5/promisesAsynch.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+
+// my own asynchronous function
+function azmiReadFile(cb){
+    fs.readFile("a.txt", "utf-8", function(err, data){
+        cb(data)
+    });
+}
+
+//calback function to call
+
+function onDone(data){
+    console.log(data)
+}
+
+azmiReadFile(onDone)
