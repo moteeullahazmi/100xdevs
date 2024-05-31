62699b09609a5f507c832c1a9294f1e1f55ca2b8
Moteeullah <MY_NAME@example.com>
2024-05-31 10:42:03 +0530

    week 2.1

---
+++ MERN/week 02/2.1/callback/setTimeout.js
@@ -0,0 +1,11 @@
+setTimeout(azmi,1000,2)
+
+console.log("first print")
+
+ function azmi(n){
+console.log("set Time", +n)
+ }
+
+ setTimeout(azmi,5000,1)
+
+//  setTimeout(function, delay, arg1, arg2, ...);
