e0c8575e5e45c0e1290563a370a1060ebd6b7ee0
Moteeullah <MY_NAME@example.com>
2024-06-03 16:51:33 +0530

    week02

--- MERN/week01/1.4/function/largestNumber.js
+++
@@ -1,4 +0,0 @@
-function findLargest(a,b,c){
-return Math.max(a,b,c)
-}
-console.log(findLargest(5, 10, 300)); // 
---
+++ week02/2.1/callback/sumofSomething.js
@@ -0,0 +1,30 @@
+<<<<<<< HEAD
+function sumOfSomething(a,b){
+    let square1 = square(a);
+    let square2 = square(b);
+
+    return square1+ square2;
+}
+
+let ans = sumOfSomething(1,2);
+console.log(ans)
+
+function square(n){
+    console.log("square")
+    return n*n
+=======
+function sumOfSomething(a,b){
+    let square1 = square(a);
+    let square2 = square(b);
+
+    return square1+ square2;
+}
+
+let ans = sumOfSomething(1,2);
+console.log(ans)
+
+function square(n){
+    console.log("square")
+    return n*n
+>>>>>>> week01
+}
