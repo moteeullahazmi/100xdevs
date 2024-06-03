2c770b91d38e631b8fc515fe169b27a606ffaa70
Moteeullah <MY_NAME@example.com>
2024-06-03 16:32:23 +0530

    week00

--- MERN/week02/2.1/promises/api.js
+++
@@ -1,4 +0,0 @@
-fetch('https://api.example.com/data')
-  .then(response => response.json()) // Parse JSON if fetch successful
-  .then(data => console.log(data))    // Handle the parsed data
-  .catch(error => console.error('Error:', error)); // Handle any errors
