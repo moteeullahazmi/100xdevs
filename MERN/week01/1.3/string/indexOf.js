81700bbe7dfcae893e50382372ff1d34ba74ed0e
Moteeullah Azmi <moteeullahazmi@gmial.com>
2024-06-03 16:17:26 +0530

    week01

---
+++ MERN/week01/1.3/string/indexOf.js
@@ -0,0 +1,5 @@
+function findIndexOf(str, target) {
+  console.log("Original String:", str);
+  console.log("Index:", str.indexOf(target));
+}
+findIndexOf("Hello World", "World");
---
+++ MERN/week02/2.1/callback/sumofSomething.js
@@ -0,0 +1,14 @@
+function sumOfSomething(a,b){
+    let square1 = square(a);
+    let square2 = square(b);
+
+    return square1+ square2;
+}
+
+let ans = sumOfSomething(1,2);
+console.log(ans)
+
+function square(n){
+    console.log("square")
+    return n*n
+}
